4ff0e4b88b7ee3b407f12bed3a95cb64e20ae021
Ciprian Prostoiu <prostoiuciprian@itis-molinari.eu>
2024-11-18 09:26:50 +0100

    progetto finito(forse)

--- componenti/table.js
+++ componenti/table.js
@@ -1,5 +1,6 @@
 export const tableComponent = () => {
     let data= [];
+    let data2= [];
     let inizioIndex = 0;
     let templateRow = `
         <tr class="tbl1">
@@ -17,7 +18,7 @@ export const tableComponent = () => {
 
     return {
         togliDati: (inizio, fine) => {data.splice(inizio, fine)}, 
-        setData: (dato) =>{data=dato},
+        setData: (dato) =>{data=dato;data2=dato},
         addData: (dato,compFetch) => {
             data.push(dato);
             compFetch.setData(data).then(dato => {
@@ -31,20 +32,16 @@ export const tableComponent = () => {
         },
         avanti:()=> {if ((inizioIndex + 5) <= data.length) {inizioIndex += 5}},
         indietro:()=> {if ((inizioIndex - 5) >= 0) {inizioIndex -= 5}},
+        reset_inizio: () => {inizioIndex=0},
+        dati_filtro: (new_Data) => {data2=new_Data},
         setTipo: (tip)=>{tipo=tip;},
         exportData: () => {return data;},
         render: () => {
-            const exportData = (date) => {
-                // FUNZIONE CHE FORMATTA LA DATA
-                let d = date.getDate().toString().padStart(2, '0'); // SE LEN MINORE DI 2 AGGIUNGE "0"
-                let m = (date.getMonth() + 1).toString().padStart(2, '0');
-                let y = date.getFullYear();
-                return y + "-" + m + "-" + d;
-            };
+            
             let html = ` <tr class="tbl1"><td>Indirizzo</td><td>Targa 1</td>
             <td>Targa 2</td><td>Targa 3</td><td>Data</td><td>Ora</td><td>Feriti</td><td>Morti</td></tr>`
             let c = 0;
-            data.forEach((el) => {
+            data2.forEach((el) => {
                 if (c >= inizioIndex && c < (inizioIndex + 5)){
                     let html2 = "";
                     html2 += templateRow.replace("#D1", el.name.indirizzo);
@@ -64,22 +61,5 @@ export const tableComponent = () => {
             
             parentElement.innerHTML = html;
         },
-        render_filtro:(data) =>{
-            let html = ` <tr class="tbl1"><td>Indirizzo</td><td>Targa 1</td>
-            <td>Targa 2</td><td>Targa 3</td><td>Data</td><td>Ora</td><td>Feriti</td><td>Morti</td></tr>`
-            data.forEach((el) => {
-                let html2 = "";
-                html2 += templateRow.replace("#D1", el.name.indirizzo);
-                html2 = html2.replace("#D2", el.name.targa1);
-                html2 = html2.replace("#D3", el.name.targa2);
-                html2 = html2.replace("#D4", el.name.targa3);
-                html2 = html2.replace("#D5", el.name.data);
-                html2 = html2.replace("#D6", el.name.ora);
-                html2 = html2.replace("#D7", el.name.feriti);
-                html2 = html2.replace("#D8", el.name.morti);  
-                html += html2;             
-            });
-            parentElement.innerHTML = html;
-        }
     }
 };
